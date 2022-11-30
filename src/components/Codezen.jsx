14d225ebf250b47dae185c3e8b1553be6bb14140
Anshika Goyal <78466781+Anshika-Goyal15@users.noreply.github.com>
2022-11-30 16:34:05 +0530

    Add files via upload

---
+++ src/components/Codezen.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {styled} from '@mui/material';
+
+const Head = styled('p')`
+font-size : 80px;
+margin : 18% 0 0 23%;
+font-weight: bold;
+`;
+
+
+const Codezen = () => {
+  return (
+    <Head>Welcome to CodeZen!!!</Head>
+  )
+}
+
+export default Codezen
